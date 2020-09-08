771486b3c2b6c2455b558b74b7c11cd3e8bb0f06
serg.volhin2014@yandex.ru <serg.volhin2014@yandex.ru>
2020-09-08 11:22:27 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace exam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.AppendAllLines("C:/Users/1253125/Desktop/text.txt", new string[] { "C:/Users/1253125/Desktop/text2.txt" });
+            using (StreamWriter w = File.AppendText("text.txt"))
+            {
+                w.WriteLine("?");
+            };
+        }
+    }
+}
